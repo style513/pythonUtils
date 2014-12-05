a1d18b7a8d912d1fc9fd2fc59d36ecab0f03aee8
zhengyj <867991328@qq.com>
2014-12-05 15:44:08 +0800

    添加默认颜色的选项

--- TerminaColor.py
+++ TerminaColor.py
@@ -8,21 +8,22 @@ blueColor = "blue"
 aubergineColor = "Aubergine"
 caynColor = "cayn"
 whiteColor = "white"
+defualColor = "defual"
 
 defualModel = 0
 heightLightModel = 1
 underlineModel = 4
 blinkModel = 5
 inverseModel = 7
-unSeeModel = 8
+invisibleModel = 8
 
 class terminalColor(object):
     def __init__(self):
-        self.fontColorDict = {"black":30,"red":31,"green":32,"yellow":33,"blue":34,"aubergineColor":35,"caynColor":36,"whiteColor":37}
-        self.backgroundColorDict = {"black":40,"red":41,"green":42,"yellow":43,"blue":44,"aubergineColor":45,"caynColor":46,"whiteColor":47}
+        self.fontColorDict = {"black":30,"red":31,"green":32,"yellow":33,"blue":34,"aubergine":35,"cayn":36,"white":37,"defual":38}
+        self.backgroundColorDict = {"black":40,"red":41,"green":42,"yellow":43,"blue":44,"aubergine":45,"cayn":46,"white":47,"defual":48}
         self.displayModel = defualModel
-        self.fontColor = blackColor
-        self.backgroundColor = whiteColor
+        self.fontColor = defualColor
+        self.backgroundColor = defualColor
     def start(self):
         str = '\033[%d;%d;%dm' % (int(self.displayModel), int(self.fontColorDict[self.fontColor]),int(self.backgroundColorDict[self.backgroundColor]))
         print str
@@ -31,9 +32,9 @@ class terminalColor(object):
 
 
 a = terminalColor()
-a.fontColor = greenColor
-a.backgroundColor = blackColor
-a.displayModel = unSeeModel
+# a.fontColor = greenColor
+# a.backgroundColor = blackColor
+# a.displayModel = inverseModel
 a.start()
 print '*' * 10
 print "hello world"
